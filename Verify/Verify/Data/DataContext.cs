8470b4d772453e86b06dc2130bc1ec6382028737
Vishnuvardan <vishnujvardan10@gmail.com>
2023-01-19 15:56:06 +0530

    User account essential
    Login/Signup, Verify user, Forgot password.

---
+++ Verify/Verify/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Verify.Models;
+
+namespace Verify.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Verify/Verify/Models/UserRegisterRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Verify.Models
+{
+    public class UserRegisterRequest
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required, MinLength(6),MaxLength(20, ErrorMessage = "Please enter a password with a length of 8 to 20 characters !")]
+        public string Password { get; set; } = string.Empty;
+        [Required, Compare("Password")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
